78b97cee4c61369b2126cc7799b3ca32d22f68e5
Truman Lahr <trumanlahr@gmail.com>
2017-10-28 23:04:10 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(sting) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toLowerCase())
+}
